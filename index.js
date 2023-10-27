e7d4ba084afda6759b872ae2571b0453206d1ef6
Pratham kumar <109858446+prathamkumar1812@users.noreply.github.com>
2023-10-28 04:38:16 +0530

    Update

---
+++ index.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const express = require("express");
+const connectDb = require("./db/connect.js");
+
+const PORT = process.env.PORT;
+const app = express();
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+app.use("/app/products", require("./routes/product.js"));
+
+connectDb();
+app.listen(PORT, () => console.log(`Server started at port : ${PORT}`));
